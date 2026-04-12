25b1dcfce81585df1e6b2a38677b8af135b87112
brunovitor-git05 <brunovazvitor@gmail.com>
2026-04-12 17:14:00 -0300

    Correção de bugs do código

--- Projeto1/src/Sistema.java
+++ Projeto1/src/Sistema.java
@@ -5,14 +5,10 @@ public class Sistema {
         Scanner sc = new Scanner(System.in);
         String nome;
         int numero;
-        double saldo1;
-        double saldo2;
-        double saldo3 = 0;
-        double saque1;
-        double saque2;
-        double saque3;
-        double saque4;
+        double saldo = 0;
+        double saque;
         double deposito = 0;
+        int quantidadeSaques = 3;
 
         System.out.println("\n====================CAIXA ELETRÔNICO VIRTUAL==========================");
         System.out.println("\nPrimeiro, digite o seu nome: ");
@@ -31,48 +27,35 @@ public class Sistema {
             switch (numero) {
                 case 1:
                     System.out.println("===============SALDO==================");
-                    System.out.println("O seu saldo atual é de: R$ " + deposito);
+                    System.out.println("O seu saldo atual é de: R$ " + saldo);
                     break;
                 case 2:
                     System.out.println("=============DEPÓSITO====================");
                     System.out.println("Informe o valor do depósito desejado: ");
                     deposito = sc.nextDouble();
-                    System.out.println("Depósito concluído com sucesso!\nO seu novo saldo é de: " + deposito);
+                    saldo = saldo + deposito;
+                    System.out.println("Depósito concluído com sucesso!\nO seu novo saldo é de: " + saldo);
                     break;
                 case 3:
-                    System.out.println("===============SAQUE======================");
-                    System.out.println(nome + ", o seu saldo atual é de: " + deposito);
-                    System.out.println("Para realizar um saque, informe o valor desejado: ");
-                    saque1 = sc.nextDouble();
-                    if (saque1 > 1000) {
-                        System.out.println("=========Operação Bloqueada============");
-                        System.out.println("\nO valor máximo por saque é de R$ 1.000,00, por questões de segurança.");
+                    if (quantidadeSaques <= 0){
+                        System.out.println("\n===================Operação Bloqueada========================");
+                        System.out.println("Limite de saques diários atingido.");
                         break;
-                    }
-                    saldo1 = deposito - saque1;
-                    System.out.println("O seu novo saldo é de: " + saldo1);
-                    System.out.println("Para realizar um novo saque informe o valor desejado: ");
-                    saque2 = sc.nextDouble();
-                    if (saque2> 1000) {
-                        System.out.println("===============Operação Bloqueada================");
-                        System.out.println("\nO valor máximo por saque é de R$ 1.000,00, por questões de segurança.");
-                        break;
-                    }
-                    saldo2 = saldo1 - saque2;
-                    System.out.println("O seu novo saldo é de: " + saldo2);
-                    System.out.println("Para realizar um novo saque informe o valor desejado: ");
-                    saque3 = sc.nextDouble();
-                    if (saque3 > 1000) {
-                        System.out.println("=========Operação Bloqueada============");
-                        System.out.println("\nO valor máximo por saque é de R$ 1.000,00, por questões de segurança.");
-                        break;
-                    }
-                    saldo3 = saldo2 - saque3;
-                    System.out.println("O seu novo saldo é de: " + saldo3);
-                    System.out.println("Para realizar um novo saque informe o valor desejado: ");
-                    saque4 = sc.nextDouble();
-                    System.out.println("\n===================Operação Bloqueada========================");
-                    System.out.println("Limite de saques diários atingido.");
+                    } else {
+                        System.out.println("===============SAQUE======================");
+                        System.out.println(nome + ", o seu saldo atual é de: " + saldo);
+                        System.out.println("Para realizar um saque, informe o valor desejado: ");
+                        saque = sc.nextDouble();
+                        if (saque > 1000) {
+                            System.out.println("=========Operação Bloqueada============");
+                            System.out.println("\nO valor máximo por saque é de R$ 1.000,00, por questões de segurança.");
+                            break;
+                        }
+                        saldo = saldo - saque;
+                        System.out.println("O seu novo saldo é de: " + saldo);
+                        System.out.println("Saque realizado com sucesso!");
+                        quantidadeSaques --;
+                      }
                     break;
                 case 0:
                     System.out.println("=================PROGRAMA FINALIZADO=================");

aaa9f806ec69a6ace59e7172b00c80f349cc2832
brunovitor-git05 <brunovazvitor@gmail.com>
2026-04-12 22:19:03 -0300

    Correção do código

--- Projeto2/src/ContaBancaria.java
+++ Projeto2/src/ContaBancaria.java
@@ -1,2 +1,63 @@
+import java.util.Scanner;
+import java.util.UUID;
+
 public class ContaBancaria {
+    String titular;
+    Double saldo = 50.0;
+    Scanner sc = new Scanner(System.in);
+    Double deposito;
+    Integer quantidadeSaques = 3;
+    Double saque;
+    String numero = UUID.randomUUID().toString();
+
+    void imprimir(){
+        System.out.println("\n====================SUA CONTA BANCÁRIA==========================");
+        System.out.println("Olá, seja bem vindo!");
+        System.out.println("Para criar uma conta bancária em nosso sistema, digite o seu nome abaixo: ");
+        titular = sc.nextLine();
+        System.out.println("\nMuito bem, " + titular + ". Sua conta foi criada com sucesso!");
+        System.out.println("Você ganhou um saldo de R$ 50,00 como bônus de boas-vindas.");
+        System.out.println("\n\n====================DADOS DA SUA CONTA=========================");
+        System.out.println("Nome do titular: " + titular);
+        System.out.println("Número da conta: " + numero);
+        System.out.printf("Saldo da conta: R$ %.2f ", saldo);
+    }
+
+    void depositar(){
+        System.out.println("\n\n=============DEPÓSITO====================");
+        System.out.println("Informe o valor do depósito desejado: ");
+        deposito = sc.nextDouble();
+        saldo = saldo + deposito;
+        System.out.println("Depósito concluído com sucesso!\nO seu novo saldo é de: " + saldo);
+    }
+
+    void sacar() {
+        if (quantidadeSaques <= 0) {
+            System.out.println("\n===================Operação Bloqueada========================");
+            System.out.println("Limite de saques diários atingido.");
+        } else {
+            System.out.println("===============SAQUE======================");
+            System.out.println(titular + ", o seu saldo atual é de: " + saldo);
+            System.out.println("Para realizar um saque, informe o valor desejado: ");
+            saque = sc.nextDouble();
+            if (saque > 1000) {
+                System.out.println("=========Operação Bloqueada============");
+                System.out.println("\nO valor máximo por saque é de R$ 1.000,00, por questões de segurança.");
+            }
+            saldo = saldo - saque;
+            System.out.println("O seu novo saldo é de: " + saldo);
+            System.out.println("Saque realizado com sucesso!");
+            quantidadeSaques--;
+
+        }
+    }
+    void verSaldo() {
+        System.out.println("===============SALDO==================");
+        System.out.println("O seu saldo atual é de: R$ " + saldo);
+    }
+    void sair() {
+        System.out.println("Usuário desconectado.");
+        System.out.println("=================PROGRAMA FINALIZADO=================");
+    }
 }
+
--- Projeto2/src/Main.java
+++ Projeto2/src/Main.java
@@ -1,8 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        ContaBancaria Conta = new ContaBancaria();
+        int opcao;
+        Scanner sc = new Scanner(System.in);
 
+        Conta.imprimir();
 
+        do {
+            System.out.println("\n\n===============SEU MENU====================");
+            System.out.println("1 - Ver Saldo");
+            System.out.println("2 - Depositar");
+            System.out.println("3 - Sacar");
+            System.out.println("0 - Sair");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    Conta.verSaldo();
+                    break;
+                case 2:
+                    Conta.depositar();
+                    break;
+                case 3:
+                    Conta.sacar();
+                    break;
+                case 0:
+                    Conta.sair();
+                    break;
+                default:
+                    System.out.println("==============OPÇÃO INVÁLIDA==================");
+                    System.out.println("Por favor, tente novamente.");
+            }
+        } while (opcao != 0);
     }
 }
